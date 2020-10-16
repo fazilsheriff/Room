9dd39697788f0899cf067aac4fca70a429fce853
fazil.s@lantrasoft.com <SinCos123>
2020-10-16 16:41:33 +0530

    Intail commit

--- app/src/main/java/com/example/room/MainActivity.kt
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -1,4 +1,19 @@
 package com.example.room
+/// Todo 1: Crud With UI
+/// Todo 2 : Recycler with  Room  DB
+/// Todo 3: Fragments with Room
+/// Todo 4: Room with Live Data
+/// Todo 5: Room,Retrofit and Recycller
+/// Todo 6: TRy to save Images in DB
+/// Todo 7: Multiple tables
+/// Todo 8: Crud With UI
+/// Todo 9: Crud With UI
+/// Todo 10: Crud With UI
+/// Todo 11: Crud With UI
+/// Todo 12: Crud With UI
+/// Todo 13: Crud With UI
+/// Todo 14: Crud With UI
+/// Todo 15: Crud With UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
